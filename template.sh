1ef253ea3a46065e03e399492d7c62a8ecc064ce
Vidyadhar Reddy <g.vidyadharreddy@gmail.com>
2026-05-11 15:19:17 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,15 @@
+#Creating Directories
+mkdir -p src
+mkdir -p research
+
+# Creating Files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompts.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Directories and files created successfully!"
